3cc699824c0b31d687039c1bbe33fedff8c46fd9
David Antognoli <david.antognoli@gmail.com>
2017-08-08 14:14:46 -0500

    added lap timer

---
+++ Assets/Scripts/LapTimer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTimer : MonoBehaviour
+{
+    private Text timeText;
+    private float laptTime = 0f;
+    private bool raceFinished = false;
+
+    private void Start()
+    {
+        timeText = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        UpdateLapTime();
+    }
+
+    private void UpdateLapTime()
+    {
+        if (!raceFinished)
+        {
+            laptTime += Time.deltaTime;
+            TimeSpan timeSpan = TimeSpan.FromSeconds(laptTime);
+
+            timeText.text = timeSpan.ToString(@"mm\:ss\:ff");
+        }
+    }
+
+    private void OnRaceFinished()
+    {
+        raceFinished = true;
+    }
+
+    private void OnEnable()
+    {
+        Checkpoint.RaceFinished += OnRaceFinished;   
+    }
+
+    private void OnDisable()
+    {
+        Checkpoint.RaceFinished -= OnRaceFinished;
+    }
+}
