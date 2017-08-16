f49343bfb2ceeeec51d4dced051abe3297b6ab14
David Antognoli <david.antognoli@gmail.com>
2017-08-16 13:38:24 -0500

    added some skip tls code to make things easier

--- Assets/Scripts/AzureMobileServiceClient.cs
+++ Assets/Scripts/AzureMobileServiceClient.cs
@@ -5,8 +5,10 @@ using Microsoft.WindowsAzure.MobileServices;
 
 public static class AzureMobileServiceClient
 {
+    // Either use mozroots / cert-syc to update your Unity mono certificate store,
+    // or set ignoreTls to true to ignore security certificate errors.
+    private const bool ignoreTls = true;
     private const string backendUrl = "http://unitytestmobileapp.azurewebsites.net";
-
     private static MobileServiceClient client;
 
     public static MobileServiceClient Client
@@ -15,6 +17,10 @@ public static class AzureMobileServiceClient
         {
             if (client == null)
             {
+                if (ignoreTls)
+                {
+                    System.Net.ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) => { return true; };
+                }
                 client = new MobileServiceClient(backendUrl);
             }
 
